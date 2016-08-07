ade84f7180dd47b66acd4efc2c45f40436cc2d50
Benedict Cohen <ben@benedictcohen.co.uk>
2016-08-07 14:32:08 +0100

    - Added framework target

---
+++ JSONReader/JSONReader/JSONReader.h
@@ -0,0 +1,19 @@
+//
+//  JSONReader.h
+//  JSONReader
+//
+//  Created by Benedict Cohen on 07/08/2016.
+//  Copyright © 2016 Benedict Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONReader.
+FOUNDATION_EXPORT double JSONReaderVersionNumber;
+
+//! Project version string for JSONReader.
+FOUNDATION_EXPORT const unsigned char JSONReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONReader/PublicHeader.h>
+
+
